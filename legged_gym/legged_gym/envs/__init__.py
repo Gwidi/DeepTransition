dc56720e02c8bbb6b8feeaecc949795e93f9b182
Gwidi <gwidon.szczepankiewicz@student.put.poznan.pl>
2025-09-01 13:46:32 +0200

    Create robot configuration and PPO configuration for Honey Badger 40

--- legged_gym/legged_gym/envs/__init__.py
+++ legged_gym/legged_gym/envs/__init__.py
@@ -4,6 +4,8 @@ import os
 from legged_gym import LEGGED_GYM_ROOT_DIR, LEGGED_GYM_ENVS_DIR
 from .base.quadruped import Quadruped
 from .base.quadruped_config import LeggedRobotCfg, LeggedRobotCfgPPO
+from .honey_badger.hb_config import HBRobotCfg, HBRobotCfgPPO
 from legged_gym.utils.task_registry import task_registry
 task_registry.register( "quadruped", Quadruped, LeggedRobotCfg(), LeggedRobotCfgPPO())
+task_registry.register( "honey_badger", Quadruped, HBRobotCfg(), HBRobotCfgPPO())
 
