9f1394ba34587d801c4c4886be11e0a83cdcb3c8
Gwidi <gwidon.szczepankiewicz@student.put.poznan.pl>
2025-09-05 16:41:35 +0200

    Create seperate class for quadrupeds with spine to make customization easier

--- legged_gym/legged_gym/envs/__init__.py
+++ legged_gym/legged_gym/envs/__init__.py
@@ -3,11 +3,12 @@
 import os
 from legged_gym import LEGGED_GYM_ROOT_DIR, LEGGED_GYM_ENVS_DIR
 from .base.quadruped import Quadruped
+from .base.quadruped_with_spine import QuadrupedWithSpine
 from .base.quadruped_config import LeggedRobotCfg, LeggedRobotCfgPPO
 from .honey_badger.hb_config import HBRobotCfg, HBRobotCfgPPO
 from .silver_badger.sb_config import SBRobotCfg, SBRobotCfgPPO
 from legged_gym.utils.task_registry import task_registry
 task_registry.register( "quadruped", Quadruped, LeggedRobotCfg(), LeggedRobotCfgPPO())
 task_registry.register( "honey_badger", Quadruped, HBRobotCfg(), HBRobotCfgPPO())
-task_registry.register( "silver_badger", Quadruped, SBRobotCfg(), SBRobotCfgPPO())
+task_registry.register( "silver_badger", QuadrupedWithSpine, SBRobotCfg(), SBRobotCfgPPO())
 
