7b78f5e8b3886cf5ee0062c50c53f220aa626f7c
Gwidi <gwidon.szczepankiewicz@student.put.poznan.pl>
2025-10-13 21:59:53 +0200

    Add privileged obs_buffer (for critic). Hide linear velocity and heights from actor

--- legged_gym/legged_gym/envs/base/quadruped_with_spine.py
+++ legged_gym/legged_gym/envs/base/quadruped_with_spine.py
@@ -213,7 +213,19 @@ class QuadrupedWithSpine(BaseTask):
         """
         #print(self.max_vel.unsqueeze(1).size(), self.actions.size())
         if "CPG" in self.cfg.control.control_type:
-            self.obs_buf = torch.cat((  self.base_lin_vel * self.obs_scales.lin_vel,
+            self.obs_buf = torch.cat((self.base_ang_vel  * self.obs_scales.ang_vel,
+                                    self.projected_gravity,
+                                    self.commands[:, :3] * self.commands_scale,
+                                    (self.dof_pos - self.default_dof_pos) * self.obs_scales.dof_pos,
+                                    self.dof_vel * self.obs_scales.dof_vel,
+                                    self.actions,
+                                    self.contact_forces[:, self.feet_indices, 2] > 1.,
+                                    (self._cpg.X[:,0,:] - ((self._cpg.mu_up[0]+ self._cpg.mu_low[0]) / 2)) * self.obs_scales.dof_pos,
+                                    (self._cpg.X[:,1,:] - np.pi) * 1/np.pi,
+                                    self._cpg.X_dot[:,0,:] * 1/30, 
+                                    (self._cpg.X_dot[:,1,:] - 15) * 1/30,
+                                    ),dim=-1)
+            self.privileged_obs_buf = torch.cat((  self.base_lin_vel * self.obs_scales.lin_vel,
                                     self.base_ang_vel  * self.obs_scales.ang_vel,
                                     self.projected_gravity,
                                     self.commands[:, :3] * self.commands_scale,
@@ -230,7 +242,7 @@ class QuadrupedWithSpine(BaseTask):
 
         if self.cfg.terrain.measure_heights:
             heights = torch.clip(self.root_states[:, 2].unsqueeze(1) - 0.5 - self.measured_heights, -1, 1.) * self.obs_scales.height_measurements
-            self.obs_buf = torch.cat((self.obs_buf, heights), dim=-1)
+            self.privileged_obs_buf = torch.cat((self.obs_buf, heights), dim=-1)
 
         if self.add_noise:
             self.obs_buf += (2 * torch.rand_like(self.obs_buf) - 1) * self.noise_scale_vec
