312d059164f20f5369039a441b255c35acd3307a
Gwidi <gwidon.szczepankiewicz@student.put.poznan.pl>
2025-12-11 15:34:54 +0100

    Add quadruped to the registry

--- legged_gym/legged_gym/envs/__init__.py
+++ legged_gym/legged_gym/envs/__init__.py
@@ -6,7 +6,9 @@ from .base.quadruped import Quadruped
 from .base.quadruped_with_spine import QuadrupedWithSpine
 from .silver_badger.sb_active_spine_config import SBActiveSpineRobotCfg, SBActiveSpineRobotCfgPPO
 from .silver_badger.sb_rigid_spine_config import SBRigidSpineRobotCfg, SBRigidSpineRobotCfgPPO
+from .base.quadruped_config import LeggedRobotCfg, LeggedRobotCfgPPO
 from legged_gym.utils.task_registry import task_registry
 task_registry.register( "silver_badger_rigid_spine", QuadrupedWithSpine, SBRigidSpineRobotCfg(), SBRigidSpineRobotCfgPPO())
 task_registry.register( "silver_badger_active_spine", QuadrupedWithSpine, SBActiveSpineRobotCfg(), SBActiveSpineRobotCfgPPO())
+task_registry.register( "quadruped", Quadruped, LeggedRobotCfg(), LeggedRobotCfgPPO())
 
