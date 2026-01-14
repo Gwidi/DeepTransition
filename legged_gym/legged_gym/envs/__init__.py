2427129f018e8df08da904a24befea39fe7c0e19
Gwidi <gwidon.szczepankiewicz@student.put.poznan.pl>
2026-01-14 14:34:58 +0100

    fix env init file

--- legged_gym/legged_gym/envs/__init__.py
+++ legged_gym/legged_gym/envs/__init__.py
@@ -5,6 +5,7 @@ from legged_gym import LEGGED_GYM_ROOT_DIR, LEGGED_GYM_ENVS_DIR
 from .base.quadruped import Quadruped
 from .base.quadruped_config import LeggedRobotCfg, LeggedRobotCfgPPO
 from legged_gym.utils.task_registry import task_registry
+from .base.quadruped_with_spine import QuadrupedWithSpine
 from .silver_badger.sb_active_spine_config import SBActiveSpineRobotCfg, SBActiveSpineRobotCfgPPO
 from .silver_badger.sb_rigid_spine_config import SBRigidSpineRobotCfg, SBRigidSpineRobotCfgPPO
 task_registry.register( "quadruped", Quadruped, LeggedRobotCfg(), LeggedRobotCfgPPO())
