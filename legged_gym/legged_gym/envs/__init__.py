d6eab252638d4dea954c169ebb324a9e5edebcd7
Gwidi <gwidon.szczepankiewicz@student.put.poznan.pl>
2026-06-17 12:10:31 +0200

    Add spine ablation and direct configuration classes for QuadrupedWithSpine

--- legged_gym/legged_gym/envs/__init__.py
+++ legged_gym/legged_gym/envs/__init__.py
@@ -6,9 +6,31 @@ from .base.quadruped import Quadruped
 from .base.quadruped_with_spine import QuadrupedWithSpine
 from .silver_badger.sb_active_spine_config import SBActiveSpineRobotCfg, SBActiveSpineRobotCfgPPO
 from .silver_badger.sb_rigid_spine_config import SBRigidSpineRobotCfg, SBRigidSpineRobotCfgPPO
+from .silver_badger.sb_spine_ablation_configs import (
+    SBSpineDirectCfg,
+    SBSpineDirectCfgPPO,
+    SBSpinePhaseLockedBaseCfg,
+    SBSpinePhaseLockedBaseCfgPPO,
+    SBSpinePhaseLockedHalfPiCfg,
+    SBSpinePhaseLockedHalfPiCfgPPO,
+    SBSpinePhaseLockedNegHalfPiCfg,
+    SBSpinePhaseLockedNegHalfPiCfgPPO,
+    SBSpinePhaseLockedPiCfg,
+    SBSpinePhaseLockedPiCfgPPO,
+    SBSpineRigidCfg,
+    SBSpineRigidCfgPPO,
+    SBSpineUncoupledCfg,
+    SBSpineUncoupledCfgPPO,
+)
 from .base.quadruped_config import LeggedRobotCfg, LeggedRobotCfgPPO
 from legged_gym.utils.task_registry import task_registry
 task_registry.register( "silver_badger_rigid_spine", QuadrupedWithSpine, SBRigidSpineRobotCfg(), SBRigidSpineRobotCfgPPO())
 task_registry.register( "silver_badger_active_spine", QuadrupedWithSpine, SBActiveSpineRobotCfg(), SBActiveSpineRobotCfgPPO())
+task_registry.register( "silver_badger_spine_ablation_rigid", QuadrupedWithSpine, SBSpineRigidCfg(), SBSpineRigidCfgPPO())
+task_registry.register( "silver_badger_spine_phase_rr_0", QuadrupedWithSpine, SBSpinePhaseLockedBaseCfg(), SBSpinePhaseLockedBaseCfgPPO())
+task_registry.register( "silver_badger_spine_phase_rr_neg_half_pi", QuadrupedWithSpine, SBSpinePhaseLockedNegHalfPiCfg(), SBSpinePhaseLockedNegHalfPiCfgPPO())
+task_registry.register( "silver_badger_spine_phase_rr_half_pi", QuadrupedWithSpine, SBSpinePhaseLockedHalfPiCfg(), SBSpinePhaseLockedHalfPiCfgPPO())
+task_registry.register( "silver_badger_spine_phase_rr_pi", QuadrupedWithSpine, SBSpinePhaseLockedPiCfg(), SBSpinePhaseLockedPiCfgPPO())
+task_registry.register( "silver_badger_spine_uncoupled", QuadrupedWithSpine, SBSpineUncoupledCfg(), SBSpineUncoupledCfgPPO())
+task_registry.register( "silver_badger_spine_direct", QuadrupedWithSpine, SBSpineDirectCfg(), SBSpineDirectCfgPPO())
 task_registry.register( "quadruped", Quadruped, LeggedRobotCfg(), LeggedRobotCfgPPO())
-
